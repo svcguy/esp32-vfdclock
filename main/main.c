d15d08f513edba5adc41e95e07dae820f6453a74
Andy <5302949+svcguy@users.noreply.github.com>
2018-12-03 21:11:45 -0600

    Add Debug Setup

--- main/main.c
+++ main/main.c
@@ -1,42 +1,31 @@
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
-}
+static const char* TAG = "app_main";
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "Starting app_main");
     nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+    
+    ESP_ERROR_CHECK( gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT) );
+    ESP_ERROR_CHECK( gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT) );
+    ESP_ERROR_CHECK( gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT) );
+    
+    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_0, 0) );
+    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_2, 0) );
+    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_4, 0) );
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
+    ESP_LOGI(TAG, "Starting app_main superloop");
+    bool level=0;
+    while(1)
+    {
+        ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_4, level));
         level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+        vTaskDelay(500/portTICK_PERIOD_MS);
     }
 }
 
