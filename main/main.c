0eb0179594642b250b602599fa0462a8d2ad4b5b
Andy <5302949+svcguy@users.noreply.github.com>
2018-12-03 22:53:32 -0600

    Inital project setup

--- main/main.c
+++ main/main.c
@@ -1,31 +1,59 @@
+/* main.c
+ *
+ * Component of VFD Clock Project
+ * 
+ * Andy Josephson 12/2018
+ */
+
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#define RD_LED_PIN  GPIO_NUM_0
+#define GR_LED_PIN  GPIO_NUM_2
+#define BL_LED_PIN  GPIO_NUM_4
+#define LED_ON      1
+#define LED_OFF     0
+
 static const char* TAG = "app_main";
+
+static void heartbeat_task(void* arg);
+static void gpio_setup(void);
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Starting app_main");
     nvs_flash_init();
-    
-    ESP_ERROR_CHECK( gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT) );
-    ESP_ERROR_CHECK( gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT) );
-    ESP_ERROR_CHECK( gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT) );
-    
-    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_0, 0) );
-    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_2, 0) );
-    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_4, 0) );
+    gpio_setup();
 
-    ESP_LOGI(TAG, "Starting app_main superloop");
-    bool level=0;
+    ESP_LOGI(TAG, "Creating tasks");
+    xTaskCreate(heartbeat_task, "task_hrtbt", 1024, NULL, 0, NULL);
+
     while(1)
     {
-        ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_4, level));
-        level = !level;
-        vTaskDelay(500/portTICK_PERIOD_MS);
     }
 }
 
+static void heartbeat_task(void* arg)
+{
+    bool level = 0;
+    while(1)
+    {
+        gpio_set_level( BL_LED_PIN, level);
+        vTaskDelay(500/portTICK_RATE_MS);
+        level = !level;
+    }
+}
+
+static void gpio_setup(void)
+{
+    ESP_ERROR_CHECK( gpio_set_direction(BL_LED_PIN, GPIO_MODE_OUTPUT) );
+    ESP_ERROR_CHECK( gpio_set_direction(GR_LED_PIN, GPIO_MODE_OUTPUT) );
+    ESP_ERROR_CHECK( gpio_set_direction(RD_LED_PIN, GPIO_MODE_OUTPUT) );
+    
+    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_0, LED_OFF) );
+    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_2, LED_OFF) );
+    ESP_ERROR_CHECK( gpio_set_level(GPIO_NUM_4, LED_OFF) );
+}
